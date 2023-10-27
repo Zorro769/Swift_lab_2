4948195aad0ca6eb14c3f1f4b56df1e9f21d01ea
Dmytro Mytsko <d.mytsko@nltu.lviv.ua>
2023-10-27 15:50:21 +0300

    lab 2

---
+++ Lab_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_1
+//
+//  Created by Admin on 10/14/23.
+//  Copyright © 2023 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
